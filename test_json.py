6331d4c45d1f365eeffca5359fbadc746379fee3
Arc7icWolf <162563901+Arc7icWolf@users.noreply.github.com>
2024-11-04 00:03:03 +0100

    Update test_json.py
    formatted with black

--- test_json.py
+++ test_json.py
@@ -2,6 +2,7 @@ import requests
 import json
 from tabulate import tabulate
 
+
 # Send request, get response
 def get_cards():
     response = requests.get("https://api.splinterlands.io/cards/get_details")
@@ -36,10 +37,20 @@ def main():
                 abilities = stats["abilities"][level]
                 if len(abilities) != 0:
                     ability_list.append(abilities)
-                stats_list.append([attack, ranged, magic, armor, health, speed, list(ability_list)])
+                stats_list.append(
+                    [attack, ranged, magic, armor, health, speed, list(ability_list)]
+                )
             with open("table.md", "a", encoding="utf-8") as f:
                 title = f"{name} - {type} - {mana} mana - {color}"
-                headers = ["Melee", "Ranged", "Magic", "Armor", "Health", "Speed", "Abilities"]
+                headers = [
+                    "Melee",
+                    "Ranged",
+                    "Magic",
+                    "Armor",
+                    "Health",
+                    "Speed",
+                    "Abilities",
+                ]
 
                 markdown_table = tabulate(stats_list, headers, tablefmt="github")
                 f.write(title + "\n" + markdown_table + "\n\n")
@@ -53,10 +64,20 @@ def main():
             health = stats["health"]
             speed = stats["speed"]
             abilities = stats.get("abilities", [])
-            stats_list.append([attack, ranged, magic, armor, health, speed, ability_list])
+            stats_list.append(
+                [attack, ranged, magic, armor, health, speed, ability_list]
+            )
             with open("table.md", "a", encoding="utf-8") as f:
                 title = f"{name} - {type} - {mana} mana - {color}"
-                headers = ["Melee", "Ranged", "Magic", "Armor", "Health", "Speed", "Abilities"]
+                headers = [
+                    "Melee",
+                    "Ranged",
+                    "Magic",
+                    "Armor",
+                    "Health",
+                    "Speed",
+                    "Abilities",
+                ]
 
                 markdown_table = tabulate(stats_list, headers, tablefmt="github")
                 f.write(title + "\n" + markdown_table + "\n\n")
@@ -64,4 +85,3 @@ def main():
 
 if __name__ == "__main__":
     main()
-    
