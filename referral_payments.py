6402cc4c55521944713333d101334d27ac6cf936
Arc7icWolf <162563901+Arc7icWolf@users.noreply.github.com>
2024-11-06 19:44:58 +0100

    Update referral_payments.py

--- referral_payments.py
+++ referral_payments.py
@@ -11,15 +11,18 @@ def get_referral_payments(payments_count, player):
 
 def main():
     player_name = "arc7icwolf"
+    # First, get number of referral payments received by the target account
     payments = get_referral_payments(1, player_name)
     payments_count = payments["count"]
+    # Then, get all referral payments for the target player
     payments = get_referral_payments(payments_count, player_name)
     payments_purchases = payments["purchases"]
     total_payments = 0
     for payment in payments_purchases:
         affiliate_payment = payment["affiliate_payment"]
+        # Split "credits" from amount received and sum to total payments received
         total_payments += float(affiliate_payment.split()[0])
-    total_payments_in_dollar = total_payments / 1000
+    total_payments_in_dollar = total_payments / 1000 # Get value in dollars
     print(f"{total_payments_in_dollar:.2f} dollars from {payments_count} referral purchases")
 
 
